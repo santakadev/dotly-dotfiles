881fa73e14dbc6f1448ff48b7e5f7515264b23ba
Dani Santamaría <santaka87@gmail.com>
2021-05-26 15:02:01 +0200

    nnn: add configuration

--- shell/exports.sh
+++ shell/exports.sh
@@ -7,6 +7,11 @@ export FZF_DEFAULT_OPTS='
   --reverse
 '
 
+BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+export NNN_PLUG='j:z;f:fzcd;p:preview-tui-ext'
+export NNN_FIFO=/tmp/nnn.fifo
+
 export path=(
   "$HOME/bin"
   "$DOTLY_PATH/bin"
